b69d0596ccb1736dcaea1c463703582375f06a2f
LeThiHongNgot <lehongngot17102003@gmail.com>
2023-11-25 15:56:14 +0700

    update

--- FootNet/Program.cs
+++ FootNet/Program.cs
@@ -1,21 +1,30 @@
+using FootNet.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<QLNetContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QLNet")));
 
-var app = builder.Build();
+builder.Services.AddCors(p => p.AddPolicy("MyCors", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build(); // This line should be the last line for building the application
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseCors("MyCors");
 app.UseRouting();
 
 app.UseAuthorization();
