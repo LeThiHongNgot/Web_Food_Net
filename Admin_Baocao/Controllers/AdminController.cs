ffa01a96f4345159d7d4a99367891a9230bbe32c
ziysqorn <tranduyquan2003@gmail.com>
2023-11-27 16:13:20 +0700

    fix-navbar

---
+++ Admin_Baocao/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Admin_Baocao.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Admin_Baocao.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Admin_Baocao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Admin_Baocao.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
