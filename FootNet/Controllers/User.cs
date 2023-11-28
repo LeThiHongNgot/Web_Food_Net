fb281310c0a4db4b3219cdc9b06afe26773dd74b
LeThiHongNgot <lehongngot17102003@gmail.com>
2023-11-28 13:03:11 +0700

    update

---
+++ FootNet/Controllers/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FootNet.ViewModels;
+namespace FootNet.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ FootNet/ViewModels/AddUserViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FootNet.ViewModels
+{
+
+    public class AddUserViewModels
+    {
+
+        [Required]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Vui lòng nhập Email")]
+        [RegularExpression("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", ErrorMessage = "Email của bạn sai định dạng")]
+        public string? Email { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$", ErrorMessage = "Mật khẩu phải chứa ít nhất một chữ hoa, một ký tự đặc biệt và một số.")]
+
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu không khớp vui lòng nhập lại")]
+        public string? Verify { get; set; }
+    }
+}
